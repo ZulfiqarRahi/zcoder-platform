b4e90cd88a8581359d7720b7c18d4cca352ed3b0
Zulfiqar ali <xulfikarali4@gmail.com>
2025-06-08 17:16:45 +0530

    Update console.js

--- routes/console.js
+++ routes/console.js
@@ -4,10 +4,8 @@ const Question=require("../models/question")
 const {Usercheck}=require("../middleware/authentication")
 const router = Router();
 router.use(Usercheck);
-// GET /editor → render the editor page
+
 router.get("/editor", (req, res) => {
-  // If you have any authentication middleware, you can insert it here.
-  // e.g. router.get("/", checkForAuthentication, (req, res) => { … });
   res.render("editor"); 
 });
 router.get("/problem",async (req,res)=>{
@@ -19,7 +17,6 @@ router.get("/problem",async (req,res)=>{
 router.post("/editor", (req, res) => {
   const { title, body, output } = req.body;
 
-  // Render editor page with the question data
   res.render("editor", {
     question: {
       title,
